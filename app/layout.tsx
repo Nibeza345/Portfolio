b3b3cc24dde3cbfa602166e1fa0cf070968b2056
captain Loml <nicole.nibeza345@gmail.com>
2024-11-13 06:42:47 +0200

    updated version

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import {Sora} from "next/font/google";
 import "./globals.css";
 import ResponsiveNav from "@/Components/Home/Navbar/ResponsiveNav";
 import Footer from "@/Components/Home/Footer/Footer";
+import ScrollToTop from "@/Components/Helper/ScrollToTop";
+ import AnimatedCursor from "react-animated-cursor" 
 
 const font = Sora({
   weight:['100', '200', '300', '400', '500', '600', '700', '800'],
@@ -22,10 +24,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}> 
+        <div className="">
+
+        </div>
         <ResponsiveNav/>
         {children}
          </body>
          <Footer/>
+         <ScrollToTop/>
     </html>
   );
 }
